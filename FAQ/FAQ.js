cd062ba798464d99a24d0ae359c787a1fecb085e
Julian Castro <jcas2tur@gmail.com>
2023-03-30 21:06:10 -0400

    favicons

--- FAQ/FAQ.js
+++ FAQ/FAQ.js
@@ -1,6 +1,6 @@
 $(window).load(function () {
   var body = $("body"),
-    universe = $("#universe"),
+    // universe = $("#universe"),
     solarsys = $("#solar-system");
 
   var init = function () {
@@ -14,19 +14,19 @@ $(window).load(function () {
       });
   };
 
-  var setView = function (view) {
-    universe.removeClass().addClass(view);
-  };
+  // var setView = function (view) {
+  //   universe.removeClass().addClass(view);
+  // };
 
-  $("#toggle-data").click(function (e) {
-    body.toggleClass("data-open data-close");
-    e.preventDefault();
-  });
+  // $("#toggle-data").click(function (e) {
+  //   body.toggleClass("data-open data-close");
+  //   e.preventDefault();
+  // });
 
-  $("#toggle-controls").click(function (e) {
-    body.toggleClass("controls-open controls-close");
-    e.preventDefault();
-  });
+  // $("#toggle-controls").click(function (e) {
+  //   body.toggleClass("controls-open controls-close");
+  //   e.preventDefault();
+  // });
 
   $("#data a").click(function (e) {
     var ref = $(this).attr("class");
@@ -54,3 +54,7 @@ $(window).load(function () {
 
   init();
 });
+function yes() {
+  console.log("yes");
+}
+document.querySelector("#1").addEventListener("click", yes());
